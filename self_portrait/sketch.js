5b220fe65a1aa080e258eb37dc9069a1282a3ec9
moequinn <moequinn50@gmail.com>
2018-09-06 15:38:10 -0400

    changes made

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -1,8 +1,25 @@
+// self portrait 
 function setup() {
-	createCanvas(640, 360);
+	createCanvas(300, 200);
+	background(80);
+	
+    // face
+	
+	fill("darkorange");
+	ellipse(100, 100, 150); 
+
+
+    // eyes
+	fill("yellow");
+	ellipse(60 , 60, 25); // right eye
+    fill("blue")
+	ellipse(135, 60, 25); // left eye
+
+
+    // mouth
+	noFill();
+	stroke("#ffddff");
+	strokeWeight(4);
+	rect(75, 125, 50, 10, 10); 	
 }
 
-function draw() {
-	background(220);
-	ellipse(580, 305, 100);
-}  
