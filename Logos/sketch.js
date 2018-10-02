566ce72a77684120e144946782d53e523aa6bbe8
Darriel Bleasdille <42749778+darrielb23@users.noreply.github.com>
2018-10-02 15:36:23 -0400

    commit to master
    new logo project

---
+++ Logos/sketch.js
@@ -0,0 +1,26 @@
+/*
+Logo
+by Darriel
+rough draft
+mmp210 week5
+*/
+
+function setup() {
+    createCanvas(500, 500);
+
+}
+
+function draw() {
+    background(0);
+    fill(255);
+
+    textAlign(CENTER, CENTER);
+
+    textSize(100);
+
+    textFont("Comic Sans MC")
+
+    text("MEME", width/2, height/2);
+
+
+}
