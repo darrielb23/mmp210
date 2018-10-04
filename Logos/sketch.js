25c5a5368ce2d53b695acca65ea36bb61b402006
Darriel Bleasdille <42749778+darrielb23@users.noreply.github.com>
2018-10-04 15:28:54 -0400

    commit to master
    have to fix

--- Logos/sketch.js
+++ Logos/sketch.js
@@ -15,20 +15,22 @@ smoke = loadImage ("allsmoke.jpg");
 
 
 function setup() {
-    createCanvas(800, 400);
+    createCanvas(900, 350);
 
 }
 
 function draw() {
     background(0);
 
-    image(smoke, 0, 0, width, height);
-    tint(20, 60, 200);
-    fill("white");
-    textAlign(CENTER, CENTER);
-    textSize(20);
+    image(smoke, 0, 0, width, height );
+    tint(200, 200, 100);
+    fill("black")
+    rect(10, 150, 550, 50)
+    fill("pink");
+//    textAlign(CENTER, CENTER);
+    textSize(25);
     textFont("Comic Sans MC")
-    text("YEAH BRO, YOU ALREADY KNOW WE OUTCHEA LIVING THE FAST LIFE !!", width/2, height/2);
-
+    text("YEAH BRO, YOU ALREADY KNOW WE OUTCHEA LIVING THE FAST LIFE !!", 200, height/2);
+    text("example", 300,300)
 
 }
