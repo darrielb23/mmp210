0667516c405cec515b8890780931bc0b98a7ebfa
Darriel Bleasdille <42749778+darrielb23@users.noreply.github.com>
2018-10-04 15:16:52 -0400

    commit to change
    meme_one

--- Logos/sketch.js
+++ Logos/sketch.js
@@ -5,22 +5,30 @@ rough draft
 mmp210 week5
 */
 
+var smoke;
+
+function preload () {
+smoke = loadImage ("allsmoke.jpg");
+
+}
+
+
+
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(800, 400);
 
 }
 
 function draw() {
     background(0);
-    fill(255);
 
+    image(smoke, 0, 0, width, height);
+    tint(20, 60, 200);
+    fill("white");
     textAlign(CENTER, CENTER);
-
-    textSize(100);
-
+    textSize(20);
     textFont("Comic Sans MC")
-
-    text("MEME", width/2, height/2);
+    text("YEAH BRO, YOU ALREADY KNOW WE OUTCHEA LIVING THE FAST LIFE !!", width/2, height/2);
 
 
 }
