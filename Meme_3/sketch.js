b733c93c95c6b4d15f4fa2389522625b76aea2d6
Darriel Bleasdille <42749778+darrielb23@users.noreply.github.com>
2018-10-18 21:03:04 -0400

    committo master
    mem3 animations complete

---
+++ Meme_3/sketch.js
@@ -0,0 +1,123 @@
+/*
+    Meme v if
+    by Darriel
+    10.09.2018
+    conditional logic / interactive
+    mmp210 week6
+*/
+
+var smoke;
+var sq;
+var sq3;
+var sq6;
+var sq7;
+var turtleY = 0;
+var turtleSpeed = 45;
+
+function preload() {
+    smoke = loadImage("allsmoke.jpg");
+    sq = loadImage("squirtle.jpg");
+    sq3 = loadImage("sq3.jpeg");
+    sq6 = loadImage("sq6.jpg");
+    sq7 = loadImage("sq7.jpg");
+}
+
+function setup() {
+    createCanvas(930, 500);
+    image(smoke, 0, 0, width / 2, height / 2);
+}
+
+function draw() {
+    background(0);
+
+
+    // background rect for mousePress1
+
+    var bgX = 130;
+    var bgY = 40;
+    var bgWidth = 259;
+    var bgHeight = 25;
+
+
+    // background rect for mousePress2
+
+    var bgX2 = 170;
+    var bgY2 = 390;
+    var bgWidth2 = 248;
+    var bgHeight2 = 25;
+
+
+
+    //text
+
+    var textFill = 'pink';
+    var textFill2 = 'black';
+
+    //memeImage mouseX moves Right
+
+
+    if (mouseX > width / 2) {
+
+        image(sq3, 0, 0, width / 2, height / 2); // top left
+
+        image(sq6, width / 2, 0, width / 2, 145) // top right
+
+
+        textSize(15);
+        text("DROP AND GIMME 20 !!", 740, 30);
+
+
+        image(smoke, width / 2, min(140, turtleY), width / 2, height); // right half
+        turtleY += turtleSpeed;
+
+
+        if (turtleY > 171 || turtleY < 0) {
+            turtleSpeed *= -1;
+        }
+        image(sq6, 0, height / 2, width / 2, height / 2); // bottom left
+         
+         
+        fill(textFill2); 
+        textSize(18);
+        text("WHEN YOU GOTTA COMPETE FOR THE LAST SPOT ON THE TEAM", 30, 280);
+
+
+        //memeImage mouseX moves left
+
+    } else {
+        image(sq7, 0, 0, width / 2, height / 2); // top left 
+        image(smoke, width / 2, height / 2, width / 2, 0); // bottom right 
+
+        textSize(28);
+        text("WELCOME TO FAST LIFE ACADEMY", 520, 100);
+        text("SCHOOL FOR THE FAST LIFE", 100, 400);
+    }
+
+
+
+    fill(textFill);
+
+    textSize(26);
+    textFont("Impact, Charcoal, sans-serif");
+
+
+    //meme image mousePress
+
+    if (mouseIsPressed) {
+
+        image(sq, 130, 0);
+
+
+        rect(bgX, bgY, bgWidth, bgHeight) // background for words
+
+        rect(bgX2, bgY2, bgWidth2, bgHeight2) // background for words2
+
+
+        fill(textFill2);
+        text("IF YOU WANA GO FASSTTT", 133, 63);
+        text("GO SUPERRR FASSSTTTT", 172, 413);
+
+    }       
+
+
+}
